a89b0d1441fd341dfc55435959dba1ba6b9de9aa
avidLearnerInProgress <chiragshah9696@gmail.com>
2019-07-26 02:10:05 +0530

    Added solutions for week 3

---
+++ week3/bfs.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using std::vector;
+using std::queue;
+
+bool bfs(vector<vector<int> > &adj, int s, int t, vector<int> &pre, vector<int> &dist){
+    int v = adj.size();
+    vector<bool> visited(v, false);
+    queue<int> q;
+    
+    pre[s] = -1;
+    dist[s] = 0;
+    visited[s] = true;
+    q.push(s);
+    
+    while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        for(auto it = adj[curr].begin(); it != adj[curr].end(); it++){
+            if(!visited[*it]){
+                visited[*it] = true;
+                dist[*it] = dist[curr] + 1;
+                pre[*it] = curr;
+                q.push(*it);
+                if(*it == t)return true;
+            }
+        }
+    }
+    return false;
+}
+
+int distance(vector<vector<int> > &adj, int s, int t) {
+    //write your code here
+    int v = adj.size();
+    vector<int> pre(v, -1);
+    vector<int> dist(v, INT32_MAX);
+
+    if(!bfs(adj, s, t, pre, dist))
+        //std::cout<<"No path from src to dest..\n";
+        return -1;
+    return dist[t];
+}
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+    vector<vector<int> > adj(n, vector<int>());
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        std::cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
+    }
+    int s, t;
+    std::cin >> s >> t;
+    s--, t--;
+    std::cout << distance(adj, s, t);
+}
